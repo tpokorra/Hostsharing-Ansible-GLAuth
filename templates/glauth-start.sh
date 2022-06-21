9d464ad72bda4cf677f0b6de132d37642eb1b1ef
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-06-21 22:53:45 +0200

    WIP: add templates, and prepare installation

---
+++ templates/glauth-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export HOME=/home/pacs/{{pac}}/users/{{user}}
+export PATH=$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+export PID=$HOME/var/run/glauth.pid
+cd $HOME
+exec $HOME/glauth64 -c $HOME/etc/glauth.cfg >$HOME/var/log/glauth.log 2>&1 &
+echo $! > $PID
